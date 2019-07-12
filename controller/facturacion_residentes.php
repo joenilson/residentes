a44d77bc20c1e565115b9686d30e42f388632ee9
Joe Nilson <joenilson@gmail.com>
2019-07-12 01:09:50 -0400

    residentes: - Work with massive billing second changes group

--- controller/facturacion_residentes.php
+++ controller/facturacion_residentes.php
@@ -33,9 +33,14 @@ class facturacion_residentes extends residentes_controller
 {
 
     public $printExtensions;
+    public $edificaciones_tipo;
+    public $edificaciones_mapa;
     public $familia;
     public $familias;
     public $impuesto;
+    public $mapa;
+    public $padre;
+    public $padre_interior;
     public $referencia;
     public $forma_pago;
     public $loop_horas;
@@ -47,6 +52,8 @@ class facturacion_residentes extends residentes_controller
     protected function private_core()
     {
         parent::private_core();
+        $this->shared_functions();
+        $this->tratarAccion();
     }
     
     public function init()
@@ -60,15 +67,66 @@ class facturacion_residentes extends residentes_controller
         $this->impuesto = new impuesto();
         $this->loop_horas = [];
         //Creamos un array para el selector de horas para cron
-        for ($x = 0; $x < 25; $x++) {
+        for ($x = 0; $x < 24; $x++) {
             $this->loop_horas[] = str_pad($x, 2, "0", STR_PAD_LEFT);
         }
+        $this->edificaciones_tipo = new residentes_edificaciones_tipo();
+        $this->edificaciones_mapa = new residentes_edificaciones_mapa();
+        $tipos = $this->edificaciones_tipo->all();
+        $this->padre = $tipos[0];
+        $this->mapa = $this->edificaciones_mapa->get_by_field('id_tipo', $this->padre->id);
+        
     }
     
     private function shared_functions()
     {
-        
+        $extensiones = array(
+            array(
+                'name' => '001_facturacion_residentes',
+                'page_from' => __CLASS__,
+                'page_to' => __CLASS__,
+                'type' => 'head',
+                'text' => '<link rel="stylesheet" type="text/css" media="screen" href="'.FS_PATH.'plugins/residentes/view/css/bootstrap-select.min.css"/>',
+                'params' => ''
+            ),
+            array(
+                'name' => '002_facturacion_residentes',
+                'page_from' => __CLASS__,
+                'page_to' => __CLASS__,
+                'type' => 'head',
+                'text' => '<script src="'.FS_PATH.'plugins/residentes/view/js/bootstrap-select.min.js" type="text/javascript"></script>',
+                'params' => ''
+            ),
+            array(
+                'name' => '003_facturacion_residentes',
+                'page_from' => __CLASS__,
+                'page_to' => __CLASS__,
+                'type' => 'head',
+                'text' => '<script src="'.FS_PATH.'plugins/residentes/view/js/i18n/defaults-es_ES.min.js" type="text/javascript"></script>',
+                'params' => ''
+            ),
+            
+        );
+
+        foreach ($extensiones as $ext) {
+            $fsext0 = new fs_extension($ext);
+            if (!$fsext0->save()) {
+                $this->new_error_msg('Imposible guardar los datos de la extensión ' . $ext['name'] . '.');
+            }
+        }
     }
+    
+    public function tratarAccion()
+    {
+        $accion = $this->filter_request('accion');
+        switch($accion) {
+            case "nueva_programacion":
+                $this->template = 'block/nueva_programacion_facturacion';
+                break;
+            default:
+                break;
+        }
+    }  
     
     
 }
